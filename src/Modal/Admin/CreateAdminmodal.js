79421f91231fa486734bb7c00aff00ec2c4f4c41
Shohruh003 <shohruhazimov0705@gmail.com>
2023-11-03 15:12:28 +0500

    Add class options

--- src/Modal/Admin/CreateAdminmodal.js
+++ src/Modal/Admin/CreateAdminmodal.js
@@ -232,11 +232,17 @@ function CreateAdminModal({ adminModal, setAdminModal }) {
                   pupil_class: event.target.value
                 })
               }} id='7' className="classNum">
-                <option defaultValue='1'>1<img src={selectIcon} /></option>
+                <option defaultValue='1'>1</option>
                 <option value='2'>2</option>
                 <option value='3'>3</option>
-                <option value='d'>d</option>
+                <option value='4'>4</option>
                 <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+                <option value='8'>8</option>
+                <option value='9'>9</option>
+                <option value='10'>10</option>
+                <option value='11'>11</option>
               </select>
               <select onChange={(event) => {
                 setUser({
